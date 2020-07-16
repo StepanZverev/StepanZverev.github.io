2d912842384cb4ec0e9e9f132eec03d3a705f7ab
StepanZverev <60239383+StepanZverev@users.noreply.github.com>
2020-07-16 15:36:31 +0300

    Add files via upload

---
+++ precache-manifest.07d3fb0b2358ab42cb65b45b609a9ee1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "628df0ad0441c85e606a9da4d86fa78d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86f9cc3052bd85c39054",
+    "url": "/static/css/main.7fe69b42.chunk.css"
+  },
+  {
+    "revision": "7990ffdcebcb79f4e579",
+    "url": "/static/js/2.624a8b86.chunk.js"
+  },
+  {
+    "revision": "ad951958ed7766763ed3e6c72917393c",
+    "url": "/static/js/2.624a8b86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86f9cc3052bd85c39054",
+    "url": "/static/js/main.47cd787a.chunk.js"
+  },
+  {
+    "revision": "33b4d27b4e6082157ff0",
+    "url": "/static/js/runtime-main.0bf2915b.js"
+  }
+]);
